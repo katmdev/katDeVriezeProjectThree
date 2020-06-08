82f6781b31b156d7518fe2e2377af2de64b6740e
katmdev <katherinedevrieze@gmail.com>
2020-06-07 22:08:11 -0400

    updated time objects to arrays for better funtionality, added minor CSS and HTML features to test JS

--- script.js
+++ script.js
@@ -25,48 +25,53 @@ alarmApp.weekDays = [
 ]
 /* get current time/date from new Date(), replace day of week and month with strings for better readability*/
 alarmApp.getCurrentTime = () => {
-    /*should this be out here or should it be global, we will find out!!*/
+    /*gets new Date() object from current time*/
     const fullDate = new Date();
     /*create object for current time-frame to facilitate looping in formatTime()*/
-    alarmApp.current = {
-        time: 'current',
-        year: fullDate.getFullYear(),
-        month: alarmApp.months[fullDate.getMonth()],
-        day: fullDate.getDate(),
-        weekday: alarmApp.weekDays[fullDate.getDay()],
-        hour: fullDate.getHours(),
-        minute: fullDate.getMinutes(),
-        second: fullDate.getSeconds(),
-        millisecond: fullDate.getTime(),
-    };
+    alarmApp.current = [
+        'current', /*time frame*/
+        fullDate.getFullYear(), /*year*/
+        fullDate.getMonth(), /*month*/
+        alarmApp.months[fullDate.getMonth()], /*month string*/
+        fullDate.getDate(), /*date*/
+        alarmApp.weekDays[fullDate.getDay()], /*day of week string*/
+        fullDate.getHours(), /*hour*/
+        fullDate.getMinutes(), /*minute*/
+        fullDate.getSeconds(), /*second*/
+        fullDate.getTime() /*millisecond*/
+    ];
     /*format time for display*/
     alarmApp.formatTime(alarmApp.current);
 };
 /*Replace single digit numbers with double digit by concatenating into string */
 alarmApp.formatTime = (time) => {
-    for (let value in time) {
-        if (typeof time[value] === 'number' && time[value] < 10)
-        time[value] = "0" + time[value];
-    };
+    const mappedArray = time.map((timeVariable) => {
+        if (typeof timeVariable === 'number' && timeVariable < 10) {
+            timeVariable = "0" + timeVariable;
+            return(timeVariable);
+        } else {
+            return(timeVariable);
+        }
+    });
     /*use newly formatted time for display*/
-    alarmApp.displayTime(time);
+    alarmApp.displayTime(mappedArray);
 };
 /*creates and adds html element with proper grammatical formatting of date*/
-alarmApp.displayTime = (timeObject) => {
+alarmApp.displayTime = (timeArray) => {
     /*allows function to be reused in display of both current and alarm times*/
     let fontSize = " ";
-    if (timeObject.time == 'current') {
+    if (timeArray[0] == 'current') {
         fontSize = `<h1>`;
     } else {
         fontSize = `<h2>`;
     };
-    const dateString = `${timeObject.weekday}, ${timeObject.month} ${timeObject.day}, ${timeObject.year}`;
+    const dateString = `${timeArray[5]}, ${timeArray[3]} ${timeArray[4]}, ${timeArray[1]}`;
     const dateContent = $(`${fontSize}`).html(dateString);
     const dateElement = $('<time>').addClass(`date__container`).append(dateContent);
-    const timeString = `${timeObject.hour}:${timeObject.minute}:${timeObject.second}`;
+    const timeString = `${timeArray[6]}:${timeArray[7]}:${timeArray[8]}`;
     const timeContent = $(`${fontSize}`).html(timeString);
     const timeElement = $('<time>').addClass(`time__container`).append(timeContent);
-    $(`#date__${timeObject.time}`).empty().append(dateElement, timeElement);
+    $(`#date__${timeArray[0]}`).empty().append(dateElement, timeElement);
 };
 /*determines button class and calls associated function*/
 alarmApp.buttonActions = (element) => {
@@ -75,35 +80,56 @@ alarmApp.buttonActions = (element) => {
         alarmApp.getAlarmTime();
     } else if (element.hasClass('close')) {
         console.log("close");
+        $('footer').animate({height: 'toggle'}, 1000,  "linear");
+        $('#date__alarm').animate({height: 'toggle'}, 1000,  "linear");
+        $('#date__current').toggleClass('together apart');
     } else if (element.hasClass('reset')) {
         console.log("reset");
     }
 };
 alarmApp.getAlarmTime = () => {
     /*create object for alarm time-frame to facilitate looping in formatTime()*/
-    alarmApp.alarm = {
-        time: 'alarm',
-        year: parseInt($('input[name=year]').val()),
-        /*months range from 0-11 in JavaScript*/
-        month: parseInt($('input[name=month]').val()) - 1,
-        day: parseInt($('input[name=day]').val()),
-        hour: parseInt($('input[name=hour]').val()),
-        minute: parseInt($('input[name=minute]').val()),
+    alarmApp.alarm = [
+        'alarm', /*[0] time frame*/
+        parseInt($('input[name=year]').val()), /*[1]year*/
+        parseInt($('input[name=month]').val()) - 1, /*[2]month numeric, month range from 0-11*/
+        /*[N/A][3 after splice]month string goes here*/ 
+        parseInt($('input[name=day]').val()), /*[3][4 after splice]date*/
+        /*[N/A][5 after splice]day of the week string goes here*/ 
+        parseInt($('input[name=hour]').val()), /*[4][6 after splice]hour*/
+        parseInt($('input[name=minute]').val()), /*[5][7 after splice]minute*/
+        "00" /*[6][8 after splice]second*/
+        /*[N/A][9 after push]millisecond*/ 
+    ];
+    /*gets new Date() object from user input*/
+    const alarmTime = new Date(alarmApp.alarm[1], alarmApp.alarm[2], alarmApp.alarm[3], alarmApp.alarm[4], alarmApp.alarm[5]);
+    /*add milliseconds to end of array*/
+    alarmApp.alarm.push(alarmTime.getTime());
+    /*add month string*/
+    alarmApp.alarm.splice(3, 0, alarmApp.months[alarmTime.getMonth()]);
+    /*add day string*/
+    alarmApp.alarm.splice(5, 0, alarmApp.weekDays[alarmTime.getDay()]);
+    console.log(alarmApp.alarm[9]);
+    console.log(alarmApp.current[9]);
+    if (alarmApp.alarm[9] > alarmApp.current[9]) {
+        /*format time for display*/
+        alarmApp.formatTime(alarmApp.alarm);
+        // alarmApp.alarmCountDown(alarmApp.alarm[9])
+        console.log("hello");
+    } else {
+        // error message
     }
-    const alarmTime = new Date(alarmApp.alarm.year, alarmApp.alarm.month, alarmApp.alarm.day, alarmApp.alarm.hour, alarmApp.alarm.minute);
-    alarmApp.alarm.milliseconds = alarmTime.getTime();
-    console.log(alarmApp.alarm);
-    console.log(alarmTime);
-    setInterval(alarmApp.alarmCountDown, 1000);
-};
-alarmApp.alarmCountDown = () => {
-    
 };
 
 alarmApp.init = () => {
     /*runs function immediately on load, and then on one second intervals, as setInterval will cause one second delay*/
     alarmApp.getCurrentTime();
     setInterval(alarmApp.getCurrentTime, 1000);
+    $('footer').on("click", function() {
+        $(this).animate({height: 'toggle'}, 1000,  "linear");
+        $('#date__alarm').animate({height: 'toggle'}, 1000, 'linear');
+        $('#date__current').toggleClass('together apart');
+    });
     /*prevent default of all buttons on click*/
     $('body').on("click", "button", function(e) {
         e.preventDefault();
